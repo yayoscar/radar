759305a714aaaad698bc3b5b35634d79213f8ff3
yayoscar <olanoscar@gmail.com>
2013-11-24 16:23:57 -0600

    Primer commit

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+$mysqli = new mysqli('localhost', 'cbtised0_admin', 'atlante3', 'cbtised0_radar');
+
+/*
+ * Esta es la forma OO "oficial" de hacerlo,
+ * AUNQUE $connect_error estaba averiado hasta PHP 5.2.9 y 5.3.0.
+ */
+if ($mysqli->connect_error) {
+    die('Error de Conexión (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+
+/*
+ * Use esto en lugar de $connect_error si necesita asegurarse
+ * de la compatibilidad con versiones de PHP anteriores a 5.2.9 y 5.3.0.
+ */
+if (mysqli_connect_error()) {
+    die('Error de Conexión (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+}
+
+echo 'Éxito... ' . $mysqli->host_info . "\n";
+
+$mysqli->close();
+?>
